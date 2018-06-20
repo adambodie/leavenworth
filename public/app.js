5597b0dfee93ed50620f605278e7661e0aca5ec3
adambodie <adam.bodie@yahoo.com>
2018-06-19 20:23:21 -0700

    Updated card vue, less, pug

--- public/app.js
+++ public/app.js
@@ -7,7 +7,8 @@ const cardArray = [
 			{english: 'Sausage', german: 'Wurst', type: 'Cuisine', flipped: false},
 			{english: 'Knight', german: 'Ritter', type: 'Person', flipped: false},
 			{english: 'Welcome', german: 'Willkommen', type: 'Greeting', flipped: false},
-			{english: 'Island', german: 'insel', type: 'Geography', flipped: false},
+			{english: 'Island', german: 'Insel', type: 'Geography', flipped: false},
+			{english: 'Tree', german: 'Baum', type: 'Plants', flipped: false},
 			{english: 'Bear', german: 'Bär', type: 'Animal', flipped: false},
 			{english: 'Bridge', german: 'Brücke', type: 'Transportation', flipped: false},
 			{english: 'Sun', german: 'Sonne', type: 'Weather', flipped: false},
@@ -18,8 +19,10 @@ const cardArray = [
 			{english: 'Highway', german: 'Autobahn', type: 'Transportation', flipped: false},
 			{english: 'Cheers', german: 'Prost', type: 'Greeting', flipped: false},
 			{english: 'Rain', german: 'Regen', type: 'Weather', flipped: false},
+			{english: 'City Hall', german: 'Rauthaus', type: 'Place', flipped: false},
 			{english: 'Waterfall', german: 'Wasserfall', type: 'Geography', flipped: false},
 			{english: 'Pipe', german: 'Rohr', type: 'Tool', flipped: false},
+			{english: 'Waitress', german: 'Kellnerin', type: 'Person', flipped: false},
 			{english: 'Chocolate', german: 'Shockolade', type: 'Cuisine', flipped: false}
 		];
 
@@ -35,7 +38,7 @@ const card = new Vue({
 		},
 		filterList: function(){
 			this.type = event.target.value;
-		}
+		},
 	},
 	computed: {
 		uniqueCardsList: function() {
@@ -46,6 +49,19 @@ const card = new Vue({
 				}
 			});
 			return types;
+		},
+		
+		shuffleCards: function() {
+			const array = cardArray;
+			let currentIndex = array.length, temporaryValue, randomIndex;
+			while (0 !== currentIndex) {
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
+			return array;
 		}
 	}
 })		
