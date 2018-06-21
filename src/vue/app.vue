172399faa4205f80e1772e560c3bc2b83f490fad
adambodie <adam.bodie@yahoo.com>
2018-06-20 19:55:31 -0700

    Added Vue Slick dependency, mock images

--- src/vue/app.vue
+++ src/vue/app.vue
@@ -1,22 +1,31 @@
 <template lang="pug">
-      .carousel
+    slick.carousel-class(ref="slick" :options="slickOptions")
+      div(v-for="photo in photos" :key="photo.index")
         div
-          img(src="http://via.placeholder.com/400x300" alt="Leavenworth")
-        div
-          img(src="http://via.placeholder.com/400x300" alt="Leavenworth")
-        div
-          img(src="http://via.placeholder.com/400x300" alt="Leavenworth")
-        div
-          img(src="http://via.placeholder.com/400x300" alt="Leavenworth")
-        div
-          img(src="http://via.placeholder.com/400x300" alt="Leavenworth")
+          img(:src="photo.title")
 </template>
+
 <script>
-	export default {
-		data() {
-			return {
-			
-			}
-		}
-	}
+import Slick from 'vue-slick'
+export default {
+  name: 'App',
+  components: { Slick },
+  data () {
+    return {
+      slickOptions: {
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+      },
+      photos: [
+        { title: '../images/test-image1.JPG', name: 'Leavenworth' },
+        { title: '../images/test-image2.JPG', name: 'Leavenworth' },
+        { title: '../images/test-image1.JPG', name: 'Leavenworth' },
+        { title: '../images/test-image4.JPG', name: 'Leavenworth' },
+        { title: '../images/test-image5.JPG', name: 'Leavenworth' },
+      ]
+    }
+  }
+}
 </script>
