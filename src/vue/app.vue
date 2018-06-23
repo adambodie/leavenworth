f87d79dfa0e825ee9ae49a7b3045345eb0fc16c8
adambodie <adam.bodie@yahoo.com>
2018-06-23 09:12:17 -0700

    Replaced Vue Slick with Owl Carousel, replaced static with axios data for images

--- src/vue/app.vue
+++ src/vue/app.vue
@@ -1,31 +1,50 @@
 <template lang="pug">
-    slick.carousel-class(ref="slick" :options="slickOptions")
-      div(v-for="photo in photos" :key="photo.index")
-        div
-          img(:src="photo.title")
+    div.owl-carousel.owl-theme
+      div(v-for="picture in pictures" :key="picture.index")
+          img(:src="'https://farm' + picture.farm + '.staticflickr.com/' + picture.server + '/' + picture.id + '_' + picture.secret + '_z.jpg'")    
 </template>
 
 <script>
-import Slick from 'vue-slick'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+
 export default {
-  name: 'App',
-  components: { Slick },
+  name: 'App', 
   data () {
     return {
-      slickOptions: {
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        centerMode: true,
-      },
-      photos: [
-        { title: '../images/test-image1.JPG', name: 'Leavenworth' },
-        { title: '../images/test-image2.JPG', name: 'Leavenworth' },
-        { title: '../images/test-image1.JPG', name: 'Leavenworth' },
-        { title: '../images/test-image4.JPG', name: 'Leavenworth' },
-        { title: '../images/test-image5.JPG', name: 'Leavenworth' },
-      ]
+      pictures: []    
     }
-  }
-}
+  },
+  mounted() {
+	const vm = this;
+	let url = `https://api.flickr.com/services/rest/
+					?method=flickr.photosets.getPhotos
+					&api_key=0c3f8d32a28de8434240115b85a28499
+					&photoset_id=72157695078896702
+					&user_id=8994820%40N07
+					&format=json&nojsoncallback=1`
+	axios.get(url)
+			.then((res) => {
+				vm.pictures = res.data.photoset.photo;
+				Vue.nextTick(function(){
+					$('.owl-carousel').owlCarousel({
+						loop: true,
+						center: true,
+						items: 1,
+						nav: true,
+						margin: 10,
+						dots: false,
+						responsive:{
+							600:{
+								items:3
+							}
+						}
+					});
+				}.bind(vm));
+			})
+		} 
+	}
 </script>
