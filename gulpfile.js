217609fa60ada8c6225931327798d2ba7bd37776
adambodie <adam.bodie@yahoo.com>
2018-07-08 19:47:04 -0700

    Cleaned up files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const concat = require('gulp-concat');
 const path = require('path');
- 
+
 gulp.task('less', () => {
   return gulp.src('./src/less/main.less')
     .pipe(less())
@@ -18,4 +18,10 @@ gulp.task('concat', () => {
 
 gulp.task('watch', function(){
   gulp.watch('./src/less/main.less', gulp.series('less'));
+});
+
+gulp.task('build-js', () => {
+  return gulp.src(['./public/app.js', './public/vue.js'])
+    .pipe(concat('build.js'))
+    .pipe(gulp.dest('./build'));
 });
