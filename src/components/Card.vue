752f4e3feeff0d71b562d04bff376d9abaec1811
adambodie <adam.m.bodie@gmail.com>
2019-02-21 17:31:46 -0800

    Added dependencies to complete testing functionality, updated Card component styles

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,8 @@
 <template>
+	<div>
 	<div class="jumbotron">
 		<h1>English-German Flash Cards</h1>
+	</div>
 		<div class="options">
 			<select v-on:change="filterList">
 				<option value="">Select type of cards...</option>
@@ -25,7 +27,7 @@
 				v-for="card in shuffleCards" 
 				:key="card.id"
 			>
-				<vue-flip :active-click="true" width="225px" height="300px" transition="0.4s" >
+				<vue-flip :active-click="true" width="259px" height="345px" transition="0.4s" >
 					<p slot="front" class="card" key="english">{{card.english}}
 						<span class="language">English</span>
 						<span class="delete-card" v-on:click="showCards(card)">X</span>
@@ -38,6 +40,7 @@
 			</li>
 		</transition-group>
 </div>
+
 </template>
 
 <script>
@@ -50,34 +53,34 @@ export default {
 	data () {
 		return {
 			cards: [
-			{english: 'Beer', german: 'Bier', type: 'Cuisine', visible: true, id: 1},
-			{english: 'Mountain', german: 'Berg', type: 'Geography', visible: true, id: 2},
-			{english: 'Goat', german: 'Ziege', type: 'Animal', visible: true, id: 3},
-			{english: 'Street', german: 'Straße', type: 'Transportation', visible: true, id: 4},
-			{english: 'River', german: 'Fluss', type: 'Geography', visible: true, id: 5},
-			{english: 'Sausage', german: 'Wurst', type: 'Cuisine', visible: true, id: 6},
-			{english: 'Knight', german: 'Ritter', type: 'Person', visible: true, id: 7},
-			{english: 'Welcome', german: 'Willkommen', type: 'Greeting', visible: true, id: 8},
-			{english: 'Island', german: 'Insel', type: 'Geography', visible: true, id: 9},
-			{english: 'Tree', german: 'Baum', type: 'Plants', visible: true, id: 10},
-			{english: 'Bear', german: 'Bär', type: 'Animal', visible: true, id: 11},
-			{english: 'Bridge', german: 'Brücke', type: 'Transportation', visible: true, id: 12},
-			{english: 'Sun', german: 'Sonne', type: 'Weather', visible: true, id: 13},
-			{english: 'Library', german: 'Bibliothek', type: 'Place', visible: true, id: 14},
-			{english: 'Goodbye', german: 'Auf Wiedersehen', type: 'Greeting', visible: true, id: 15},
-			{english: 'Flowers', german: 'Blumen', type: 'Plants', visible: true, id: 16},
-			{english: 'Nutcracker', german: 'Nussknacker', type: 'Tool', visible: true, id: 17},
-			{english: 'Clouds', german: 'Wolken', type: 'Weather', visible: true, id: 18},
-			{english: 'Highway', german: 'Autobahn', type: 'Transportation', visible: true, id: 19},
-			{english: 'Cheers', german: 'Prost', type: 'Greeting', visible: true, id: 20},
-			{english: 'Blackbird', german: 'Amsel', type: 'Animal', visible: true, id: 21},
-			{english: 'Rain', german: 'Regen', type: 'Weather', visible: true, id: 22},
-			{english: 'City Hall', german: 'Rauthaus', type: 'Place', visible: true, id: 23},
-			{english: 'Waterfall', german: 'Wasserfall', type: 'Geography', visible: true, id: 24},
-			{english: 'Pipe', german: 'Rohr', type: 'Tool', visible: true, id: 25},
-			{english: 'Waitress', german: 'Kellnerin', type: 'Person', visible: true, id: 26},
-			{english: 'Icicle', german: 'Eiszapfen', type: 'Tool', visible: true, id: 27},
-			{english: 'Chocolate', german: 'Shockolade', type: 'Cuisine', visible: true, id: 28}
+				{english: 'Beer', german: 'Bier', type: 'Cuisine', visible: true, id: 1},
+				{english: 'Mountain', german: 'Berg', type: 'Geography', visible: true, id: 2},
+				{english: 'Goat', german: 'Ziege', type: 'Animal', visible: true, id: 3},
+				{english: 'Street', german: 'Straße', type: 'Transportation', visible: true, id: 4},
+				{english: 'River', german: 'Fluss', type: 'Geography', visible: true, id: 5},
+				{english: 'Sausage', german: 'Wurst', type: 'Cuisine', visible: true, id: 6},
+				{english: 'Knight', german: 'Ritter', type: 'Person', visible: true, id: 7},
+				{english: 'Welcome', german: 'Willkommen', type: 'Greeting', visible: true, id: 8},
+				{english: 'Island', german: 'Insel', type: 'Geography', visible: true, id: 9},
+				{english: 'Tree', german: 'Baum', type: 'Plants', visible: true, id: 10},
+				{english: 'Bear', german: 'Bär', type: 'Animal', visible: true, id: 11},
+				{english: 'Bridge', german: 'Brücke', type: 'Transportation', visible: true, id: 12},
+				{english: 'Sun', german: 'Sonne', type: 'Weather', visible: true, id: 13},
+				{english: 'Library', german: 'Bibliothek', type: 'Place', visible: true, id: 14},
+				{english: 'Goodbye', german: 'Auf Wiedersehen', type: 'Greeting', visible: true, id: 15},
+				{english: 'Flowers', german: 'Blumen', type: 'Plants', visible: true, id: 16},
+				{english: 'Nutcracker', german: 'Nussknacker', type: 'Tool', visible: true, id: 17},
+				{english: 'Clouds', german: 'Wolken', type: 'Weather', visible: true, id: 18},
+				{english: 'Highway', german: 'Autobahn', type: 'Transportation', visible: true, id: 19},
+				{english: 'Cheers', german: 'Prost', type: 'Greeting', visible: true, id: 20},
+				{english: 'Blackbird', german: 'Amsel', type: 'Animal', visible: true, id: 21},
+				{english: 'Rain', german: 'Regen', type: 'Weather', visible: true, id: 22},
+				{english: 'City Hall', german: 'Rauthaus', type: 'Place', visible: true, id: 23},
+				{english: 'Waterfall', german: 'Wasserfall', type: 'Geography', visible: true, id: 24},
+				{english: 'Pipe', german: 'Rohr', type: 'Tool', visible: true, id: 25},
+				{english: 'Waitress', german: 'Kellnerin', type: 'Person', visible: true, id: 26},
+				{english: 'Icicle', german: 'Eiszapfen', type: 'Tool', visible: true, id: 27},
+				{english: 'Chocolate', german: 'Shockolade', type: 'Cuisine', visible: true, id: 28}
 			],
 			type: '',
 			visible: true,
--- test/Card.test.js
+++ test/Card.test.js
@@ -1,3 +1,31 @@
+import Vue from 'vue'
+import Card from '../src/components/Card.vue'
+import { shallowMount } from '@vue/test-utils'
+
+describe('Card.test.js', () => {
+	let cmp, vm
+	
+	beforeEach(() => {
+		cmp = shallowMount(Card, {})
+		cmp.setData({cards: [
+			{english: 'Beer', german: 'Bier', type: 'Cuisine', flipped: false, visible: true},
+			{english: 'Mountain', german: 'Berg', type: 'Geography', flipped: false, visible: true},
+			{english: 'Goat', german: 'Ziege', type: 'Animal', flipped: false, visible: true}
+		], 
+		checked: false, type: '', visible: true})
+	});
+	
+	it('has the expected html structure', () => {
+		expect(cmp.element).toMatchSnapshot();
+	});
+	it('should have a length of 3', () => {
+		expect(cmp.vm.uniqueCardsList.length).toBe(3)
+	});
+	it('should contain Animal', () => {
+		expect(cmp.vm.uniqueCardsList).toContain('Animal')
+	});	
+})
+
 function sum(a, b) {
     return a + b;
   }
